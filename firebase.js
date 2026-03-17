52b6ad3967fd457993b14adf06271e7f8ad8c793
earnmoneyindia <praisenlaudjesus@gmail.com>
2026-03-17 09:57:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Firebase config
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDXws-cPYl9uoLTDuIY8iBCzYmgs6z5BGs",
   authDomain: "spinwinindia.firebaseapp.com",
   projectId: "spinwinindia",
-  storageBucket: "spinwinindia.firebasestorage.app",
+  storageBucket: "spinwinindia.appspot.com",
   messagingSenderId: "413087280761",
-  appId: "1:413087280761:web:2f37b4a1a60a50094baea8",
-  measurementId: "G-GKGVS527MR"
+  appId: "1:413087280761:web:2f37b4a1a60a50094baea8"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-// make database global
-window.db = db;
+export const db = getFirestore(app);
+export const auth = getAuth(app);
