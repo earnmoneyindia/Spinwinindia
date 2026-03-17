05a81c358e2f8e083f524134f0c93605fb4eb22f
earnmoneyindia <praisenlaudjesus@gmail.com>
2026-03-17 09:58:21 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,69 +1,46 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } 
+import { signInWithPopup, GoogleAuthProvider, signOut } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import { doc, setDoc, getDoc } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-const firebaseConfig = {
-
-apiKey: "AIzaSyDXws-cPYl9uoLTDuIY8iBCzYmgs6z5BGs",
-
-authDomain: "spinwinindia.firebaseapp.com",
-
-projectId: "spinwinindia",
-
-storageBucket: "spinwinindia.appspot.com",
-
-messagingSenderId: "413087280761",
-
-appId: "1:413087280761:web:2f37b4a1a60a50094baea8"
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-const auth = getAuth(app);
+import { auth, db } from "./firebase.js";
 
 const provider = new GoogleAuthProvider();
 
 
+// LOGIN
 window.googleLogin = async function(){
 
-const result = await signInWithPopup(auth,provider);
+const result = await signInWithPopup(auth, provider);
 
 const user = result.user;
 
-const userRef = doc(db,"users",user.uid);
+const ref = doc(db,"users",user.uid);
 
-const snap = await getDoc(userRef);
+const snap = await getDoc(ref);
 
 if(!snap.exists()){
 
-await setDoc(userRef,{
-
+await setDoc(ref,{
 name:user.displayName,
-
 email:user.email,
-
 coins:100,
-
 wallet:0,
-
 date:new Date().toISOString()
-
 });
 
 }
 
 window.location.href="index.html";
-
 }
+
+
+// LOGOUT
+window.logout = function(){
+
+signOut(auth).then(()=>{
+window.location.href="login.html";
+});
+
+  }
