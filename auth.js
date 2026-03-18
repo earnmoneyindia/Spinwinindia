d6a8404cc978e765733da26c049d9048a8436b0b
earnmoneyindia <praisenlaudjesus@gmail.com>
2026-03-19 03:27:08 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,37 +1,15 @@
-import { signInWithPopup, GoogleAuthProvider, signOut }
+import { getAuth, signInWithPopup, GoogleAuthProvider }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { doc, setDoc, getDoc }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import { auth, db } from "./firebase.js";
-
+const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
-window.googleLogin = async function(){
+window.login = async function(){
 
-const result = await signInWithPopup(auth, provider);
-const user = result.user;
+const result = await signInWithPopup(auth,provider);
 
-const ref = doc(db,"users",user.uid);
-const snap = await getDoc(ref);
-
-if(!snap.exists()){
-await setDoc(ref,{
-name:user.displayName,
-email:user.email,
-coins:100,
-wallet:0,
-referral:user.uid.slice(0,6),
-date:new Date().toISOString()
-});
-}
+localStorage.setItem("user", result.user.displayName);
 
 window.location.href="index.html";
-}
 
-window.logout = function(){
-signOut(auth).then(()=>{
-window.location.href="login.html";
-});
 }
