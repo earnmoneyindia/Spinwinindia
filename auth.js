518bc6b66d7c3a3eb9129210b1e4eb48b78d81d9
earnmoneyindia <praisenlaudjesus@gmail.com>
2026-03-18 19:41:59 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,46 +1,37 @@
-import { signInWithPopup, GoogleAuthProvider, signOut } 
+import { signInWithPopup, GoogleAuthProvider, signOut }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { doc, setDoc, getDoc } 
+import { doc, setDoc, getDoc }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import { auth, db } from "./firebase.js";
 
 const provider = new GoogleAuthProvider();
 
-
-// LOGIN
 window.googleLogin = async function(){
 
 const result = await signInWithPopup(auth, provider);
-
 const user = result.user;
 
 const ref = doc(db,"users",user.uid);
-
 const snap = await getDoc(ref);
 
 if(!snap.exists()){
-
 await setDoc(ref,{
 name:user.displayName,
 email:user.email,
 coins:100,
 wallet:0,
+referral:user.uid.slice(0,6),
 date:new Date().toISOString()
 });
-
 }
 
 window.location.href="index.html";
 }
 
-
-// LOGOUT
 window.logout = function(){
-
 signOut(auth).then(()=>{
 window.location.href="login.html";
 });
-
-  }
+}
